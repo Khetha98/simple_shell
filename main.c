7053fcdaa47fbb4ca4d8a5bb89b769567fe510d4
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2023-10-18 23:44:20 +0200

    main.c

--- main.c
+++ main.c
@@ -1,25 +1,23 @@
 #include "shell.h"
 
-
-
 /**
- * main - is the entry point
- * @ac: is the arg count
- * @av: is the arg vector
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
  *
- * Return: returns 0 when success,  but 1 when error
+ * Return: 0 on success, 1 on error
  */
+
 int main(int ac, char **av)
 {
 	info_t info[] = { INFO_INIT };
 	int fd = 2;
-	
+
 	asm ("mov %1, %0\n\t"
 		"add $3, %0"
 		: "=r" (fd)
 		: "r" (fd));
 
-	
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
@@ -37,6 +35,7 @@ int main(int ac, char **av)
 				exit(127);
 			}
 			return (EXIT_FAILURE);
+
 		}
 		info->readfd = fd;
 	}
